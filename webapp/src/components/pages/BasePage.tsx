3653de939487e84adbda3c9338eefda0deb9eb0f
Cole Cherney <ccherney@uwaterloo.ca>
2022-07-12 14:39:43 -0400

    add auth0 sign on

---
+++ webapp/src/components/pages/BasePage.tsx
@@ -0,0 +1,15 @@
+import '../../App.css'
+
+
+const Home: React.FC = () => {
+    return (
+        <>
+            <div>
+                <h3>This is the Base Page where you will be sent when you are not logged in </h3>
+                <h3>Please log in </h3>
+            </div>
+        </>
+    )
+}
+
+export default Home;
