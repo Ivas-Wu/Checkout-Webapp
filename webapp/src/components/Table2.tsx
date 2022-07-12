df4820528b4cd80ba8c5275d0e8e43a6b3ddb02b
Cole Cherney <ccherney@uwaterloo.ca>
2022-07-12 01:47:25 -0400

    add another table

---
+++ webapp/src/components/Table2.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { DataGrid, GridColDef} from '@mui/x-data-grid';
+
+interface TableReqs {
+    id: Number
+}
+
+// Eventually add a union type instead like User | Receipt | Item | ... when I get around to building backend for those
+// Until then TableReqs will do
+
+export interface TableProps<T extends TableReqs> {
+    cols: GridColDef[],
+    rows: T[],
+    pageSize?: number
+    checkboxes?: boolean
+}
+
+export default function DataTable<T extends TableReqs>(props: TableProps<T> ) {
+  return (
+    <div style={{ height: 400, width: '50%' }}>
+      <DataGrid
+        rows={props.rows}
+        columns={props.cols}
+        pageSize={props.hasOwnProperty('pageSize') ? props.pageSize : 5}
+        checkboxSelection={props.hasOwnProperty('checkboxes') ? props.checkboxes : true}
+      />
+    </div>
+  );
+}
