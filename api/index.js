900acb194764b53fbb11b5f5019b0ab8211afa85
Cole Cherney <ccherney@uwaterloo.ca>
2022-07-17 15:47:11 -0400

    add a hook to backend from frontend

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 var corsOptions = {
-  origin: 'http://localhost:3000'
+  origin: 'http://localhost:3005'
 }
 // use cors options
 app.use(cors(corsOptions))
--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -11,7 +11,7 @@ root.render(
   <Auth0Provider
     domain="dev-wsk1guie.us.auth0.com"
     clientId="NHrIFafN5WBtQ8SIWhk0xIMlrrKV24va"
-    redirectUri="http://localhost:3000/home"
+    redirectUri="http://localhost:3005/home"
   >
     <App />
   </Auth0Provider>
