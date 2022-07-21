c78c9b0563fe24bb6b8f65b4ca2b7f52ef53e249
Hyunnam Shim <h4shim@uwaterloo.ca>
2022-07-21 17:20:32 -0400

    made changes to scanner

--- api/app/controllers/scanner_util/scanner.py
+++ api/app/controllers/scanner_util/scanner.py
@@ -3,7 +3,7 @@ import json
 import re
 
 PLACE_NAME = 'Store'
-
+TOTAL = 'Total'
 def ocr_receipt(filename, api_key, overlay=False, tabulate=True, language='eng'):
     """ OCR.space API request with local file.
     :param filename: Your file path & name.
@@ -35,25 +35,28 @@ def convert_to_json(receipt):
     json_receipt = {}
     lineiterator = receipt.splitlines()
     json_receipt[PLACE_NAME] = process_string(lineiterator[0])
-    for line in lineiterator:
-        name, price = match_regex(line)
+    items = re.findall("([A-Za-z0-9][A-Za-z_\s$()]*[0-9]+[\s]*\.\s*[0-9]+)", receipt)
+    for item in items:
+        name, price = match_regex(item)
         if name and price:
             json_receipt[name] = price
+            if name == TOTAL:
+                break
     json_receipt = json.dumps(json_receipt)
     return json_receipt
 
 def match_regex(string):
     name_matches = re.search("([A-Za-z0-9_\s()]*)", string)
-    price_matches = re.search("([0-9]+\.[0-9]{2})", string)
+    price_matches = re.search("([0-9\s]+\.[\s0-9]+)", string)
     if name_matches and price_matches:
-        return process_string(name_matches.group(0)), price_matches.group(0)
+        return process_string(name_matches.group(0)), process_num(price_matches.group(0))
     else:
         return "" , ""
 
 def process_string(string):
-    return_string = string.rstrip('0123456789')
-    return_string = return_string.strip('\n')
-    return_string = return_string.strip('\t')
+    return_string = string.strip()
+    return_string = return_string.strip('0123456789')
+    return_string = return_string.strip()
     return_string = remove_keywords(return_string)
     return return_string
 
@@ -63,15 +66,21 @@ def remove_keywords(string):
         return_string = ""
     if bool(re.search('(?i)(subt)', string)):
         return_string = ""
-    if bool(re.search('(?i)(total)', string)):
+    elif bool(re.search('(?i)(tax)', string)):
+        return_string = "Tax"
+    elif bool(re.search('(?i)(total)', string)) or bool(re.search('(?i)(tota)', string)):
         return_string = "Total"
-    if bool(re.search('(?i)(tip)', string)):
+    elif bool(re.search('(?i)(tip)', string)):
         return_string = ""
+    return return_string
+
+def process_num(string):
+    return_string = re.sub("\s+", "", string)
     return return_string
 
 def main():
     api_key = 'K83047060888957'
-    file_path = './app/controllers/scanner_util/sample_data/1013-receipt.jpg'
+    file_path = './app/controllers/scanner_util/sample_data/1014-receipt.jpg'
     return_file = ocr_receipt(filename=file_path, api_key=api_key, overlay=False)
     print(return_file)
     
