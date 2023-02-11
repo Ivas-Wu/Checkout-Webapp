49a6bb8668671ecaadf6675e204a9b3f0001ac47
Ivas Wu <ivas.wu@yahoo.ca>
2023-02-11 02:19:08 -0500

    S

--- webapp/src/components/auth/LogoutButton.tsx
+++ webapp/src/components/auth/LogoutButton.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import Button from '../Button';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Logout from '@mui/icons-material/Logout';
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
+  
+  const handleClose = () => {
+    localStorage.removeItem('user-id');
+    logout({ returnTo: window.location.origin });
+  };
 
   return (
-    <Button
-      buttonStyle="btn--outline"
-      onClick={() => {
-        localStorage.removeItem('user-id');
-        logout({ returnTo: window.location.origin });
-      }}
-    >
-      Log Out
-    </Button>
+    // <Button
+    //   buttonStyle="btn--outline"
+    //   onClick={handleClose}
+    // >
+    //   Log Out
+    // </Button>
+    <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+    </MenuItem>
   );
 };
 
