9ec0837945ae9dbcd77e9878576563a825cf41d2
Ivas Wu <ivas.wu@yahoo.ca>
2023-03-03 15:07:10 -0500

    target

--- webapp/src/components/pages/Stats.tsx
+++ webapp/src/components/pages/Stats.tsx
@@ -1,4 +1,5 @@
 import '../../App.css';
+import React, { useState, useEffect } from 'react';
 import Graphs, { charts } from '../Graphs';
 import 'react-widgets/styles.css';
 import { Welcome, EndCard } from './pages.styled';
@@ -9,10 +10,19 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '../Button';
+import axios from 'axios';
+import { Target, TargetCreateReq, TargetUpdateReq } from '../../types/Target';
+import { Category } from '../../types/Category';
+import { dataValue } from 'react-widgets/esm/Accessors';
 
 export interface IStatisticsPageProps {}
 
 const Statistics: React.FC<IStatisticsPageProps> = () => {
+  const [targetData, setData] = useState<Target[]>([]);
+  const [valueData, setValue] = useState<number>(0);
+  const [newValue, setNewValue] = useState<number>(0);
+  const [id, setId] = useState<number>(1);
+  const userId = Number(localStorage.getItem('user-id')!);
   const graph_width = window.innerWidth * 0.45;
   const graph_height = window.innerHeight * 0.4;
   const outer_style = {
@@ -20,8 +30,6 @@ const Statistics: React.FC<IStatisticsPageProps> = () => {
     left: '5%',
     width: '55%',
     bgcolor: 'background.paper',
-    // border: '2px solid #000',
-    // boxShadow: 24,
     marginRight: '3%',
     pt: 2,
     px: 4,
@@ -32,7 +40,6 @@ const Statistics: React.FC<IStatisticsPageProps> = () => {
     position: 'relative',
     width: graph_width,
     bgcolor: 'background.paper',
-    // border: '2px solid #000',
     marginBottom: '5%',
     marginTop: '5%',
   };
@@ -46,16 +53,63 @@ const Statistics: React.FC<IStatisticsPageProps> = () => {
     marginBottom: '5%',
   };
 
-  const budget_style2 = {
-    position: 'relative',
-    left: '5%',
-    height: '67%',
-    width: '75%',
-    bgcolor: 'bacground.paper',
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios
+      .get(`http://localhost:3000/api/targets?userId=${userId}`)
+      .then((res) => {
+        let data: Target[] = convertTargets(res.data);
+        setData(data);
+        console.log(valueData);
+        console.log(id);
+      });
   };
 
-  const submit = () => {
-    console.log("TODO")
+  function convertTargets(data: Target[]): Target[] {
+    let returnValue: Target[] = [];
+    let once = true;
+    data.forEach(function (data) {
+      const newData = {
+        id: data.id,
+        userId: data.userId,
+        category: data.category,
+        value: data.value,
+      };
+      if (once) {
+        setValue(data.value);
+        setId(data.id);
+        once = false;
+      }
+      returnValue.push(newData);
+    });
+    return returnValue;
+  }
+
+  const submitBudget = (): void => {
+    if (targetData.length != 0) {
+      let data: TargetUpdateReq = {
+        userId: userId,
+        category: Category.OTHER,
+        value: newValue,
+      };
+      // Popup Warning overwrite
+      axios.put(`http://localhost:3000/api/targets/${id}`, data).then((res) => {
+        getData();
+      });
+    } else {
+      let data: TargetCreateReq = {
+        userId: userId,
+        category: Category.OTHER,
+        value: newValue,
+      };
+      axios.post(`http://localhost:3000/api/targets/`, data).then((res) => {
+        getData();
+      });
+    }
+    setNewValue(0);
   };
 
   return (
@@ -77,6 +131,7 @@ const Statistics: React.FC<IStatisticsPageProps> = () => {
                 graph={charts.PI}
                 width={graph_width}
                 height={graph_height}
+                target={valueData}
               />
             </Box>
             <Box sx={{ ...inner_style }}>
@@ -84,6 +139,7 @@ const Statistics: React.FC<IStatisticsPageProps> = () => {
                 graph={charts.BAR}
                 width={graph_width}
                 height={graph_height}
+                target={valueData}
               />
             </Box>
             <Box sx={{ ...inner_style }}>
@@ -91,6 +147,7 @@ const Statistics: React.FC<IStatisticsPageProps> = () => {
                 graph={charts.LINE}
                 width={graph_width}
                 height={graph_height}
+                target={valueData}
               />
             </Box>
           </CardContent>
@@ -113,53 +170,54 @@ const Statistics: React.FC<IStatisticsPageProps> = () => {
               subheader="Let's (bud)get this bread 🍞"
             />
             <CardContent>
-              <Box
-                component="form"
-                sx={{
-                  '& .MuiTextField-root': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-              >
-                <div>
-                  <TextField
+              <Box>
+                <div style={{ marginBottom: '10%' }}>
+                  {/* <TextField
                     required
                     id="standard-required"
                     label="Budgeting goal"
                     defaultValue=""
                     variant="standard"
-                  />
+                  /> */}
+                  <Typography variant="body2" color="text.secondary" style={{marginBottom:'10%'}}>
+                    Set your personal budget and compare it to your current spending!
+                  </Typography>
                   <TextField
                     id="standard-number"
-                    label="Number"
+                    label="Budget Goal!"
                     type="number"
                     InputLabelProps={{
                       shrink: true,
                     }}
                     variant="standard"
+                    value={newValue}
+                    onChange={(event) => {
+                      setNewValue(Number(event.target.value));
+                    }}
                   />
                 </div>
                 <Button
-                    buttonStyle="btn--extra"
-                    buttonSize="btn--small"
-                    onClick={submit}
-                  >
-                    Submit
+                  buttonStyle="btn--extra"
+                  buttonSize="btn--medium"
+                  onClick={submitBudget}
+                >
+                  Set Budget!
                 </Button>
               </Box>
             </CardContent>
           </Card>
           <Card sx={{ ...budget_style }}>
             <CardHeader
-                title="Personal Ads ;)"
-                subheader="100% of clients would recommend"
-              />
-              <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                  Enjoy the content? You can put your own ads here! Contact us to find out more!
-                </Typography>
-              </CardContent>
-            </Card>
+              title="Personal Ads ;)"
+              subheader="100% of clients would recommend"
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                Enjoy the content? You can put your own ads here! Contact us to
+                find out more!
+              </Typography>
+            </CardContent>
+          </Card>
         </div>
       </div>
       <EndCard>Here you can view graphs with your statistics!</EndCard>
