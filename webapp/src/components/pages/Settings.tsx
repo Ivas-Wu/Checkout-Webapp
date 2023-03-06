6182dec1546ff6fa09521a57ce9d90a4f47599ad
Ivas Wu <ivas.wu@yahoo.ca>
2023-03-06 14:41:19 -0500

    Recommender

--- webapp/src/components/pages/Settings.tsx
+++ webapp/src/components/pages/Settings.tsx
@@ -41,6 +41,10 @@ const Settings: React.FC<ISettingsPagePros> = () => {
   const [totalAll, setTotalAll] = useState<number>(0);
   const [totalWeeklyAll, setTotalWeeklyAll] = useState<number>(0);
   const [totalMonthlyAll, setTotalMonthlyAll] = useState<number>(0);
+  const [totalDemo, setDemoTotal] = useState<number>(0);
+  const [totalDemoWeekly, setDemoTotalWeekly] = useState<number>(0);
+  const [avgDemo, setDemoAvg] = useState<number>(0);
+  const [avgDemoWeekly, setDemoAvgWeekly] = useState<number>(0);
   const [over, setOver] = useState<string[]>([]);
   const [selectedValue, setSelectedValue] = useState<string>('');
   const today = new Date();
@@ -114,7 +118,7 @@ const Settings: React.FC<ISettingsPagePros> = () => {
   };
 
   const getTotal = async (category?: string) => {
-    let url: string = `http://localhost:3000/api/recommender/average?userId=${userId}`;
+    let url: string = `http://localhost:3000/api/recommender/total?userId=${userId}`;
     if (category) {
       url += `&category=${category}`;
     }
@@ -124,14 +128,14 @@ const Settings: React.FC<ISettingsPagePros> = () => {
         url: url,
         headers: { 'Content-Type': 'multipart/form-data' },
       });
-      setTotal(Number(Number(response.data.average).toFixed(2)));
+      setTotal(Number(Number(response.data.total).toFixed(2)));
     } catch (error) {
       console.log(error);
     }
   };
 
   const getWeeklyTotal = async (category?: string) => {
-    let url: string = `http://localhost:3000/api/recommender/weeklyAverage?userId=${userId}`;
+    let url: string = `http://localhost:3000/api/recommender/weeklyTotal?userId=${userId}`;
     if (category) {
       url += `&category=${category}`;
     }
@@ -141,14 +145,14 @@ const Settings: React.FC<ISettingsPagePros> = () => {
         url: url,
         headers: { 'Content-Type': 'multipart/form-data' },
       });
-      setTotalWeekly(Number(Number(response.data.average).toFixed(2)));
+      setTotalWeekly(Number(Number(response.data.total).toFixed(2)));
     } catch (error) {
       console.log(error);
     }
   };
 
   const getMonthlyTotal = async (month?: number, category?: string) => {
-    let url: string = `http://localhost:3000/api/recommender/monthlyAverage?userId=${userId}`;
+    let url: string = `http://localhost:3000/api/recommender/monthlyTotal?userId=${userId}`;
     if (month) {
       url += `&month=${month}`;
     }
@@ -161,16 +165,16 @@ const Settings: React.FC<ISettingsPagePros> = () => {
         url: url,
         headers: { 'Content-Type': 'multipart/form-data' },
       });
-      setTotalMonthly(Number(Number(response.data.average).toFixed(2)));
+      setTotalMonthly(Number(Number(response.data.total).toFixed(2)));
     } catch (error) {
       console.log(error);
     }
   };
 
   const getTotalAll = async (category?: string) => {
-    let url: string = `http://localhost:3000/api/recommender/average?userId=${userId}`;
+    let url: string = `http://localhost:3000/api/recommender/total`;
     if (category) {
-      url += `&category=${category}`;
+      url += `?category=${category}`;
     }
     try {
       const response = await axios({
@@ -178,16 +182,16 @@ const Settings: React.FC<ISettingsPagePros> = () => {
         url: url,
         headers: { 'Content-Type': 'multipart/form-data' },
       });
-      setTotalAll(Number(Number(response.data.average).toFixed(2)));
+      setTotalAll(Number(Number(response.data.total).toFixed(2)));
     } catch (error) {
       console.log(error);
     }
   };
 
   const getWeeklyTotalAll = async (category?: string) => {
-    let url: string = `http://localhost:3000/api/recommender/weeklyAverage?userId=${userId}`;
+    let url: string = `http://localhost:3000/api/recommender/weeklyTotal`;
     if (category) {
-      url += `&category=${category}`;
+      url += `?category=${category}`;
     }
     try {
       const response = await axios({
@@ -195,19 +199,21 @@ const Settings: React.FC<ISettingsPagePros> = () => {
         url: url,
         headers: { 'Content-Type': 'multipart/form-data' },
       });
-      setTotalWeeklyAll(Number(Number(response.data.average).toFixed(2)));
+      setTotalWeeklyAll(Number(Number(response.data.total).toFixed(2)));
     } catch (error) {
       console.log(error);
     }
   };
 
   const getMonthlyTotalAll = async (month?: number, category?: string) => {
-    let url: string = `http://localhost:3000/api/recommender/monthlyAverage?userId=${userId}`;
+    let url: string = `http://localhost:3000/api/recommender/monthlyTotal`;
     if (month) {
-      url += `&month=${month}`;
-    }
-    if (category) {
-      url += `&category=${category}`;
+      url += `?month=${month}`;
+      if (category) {
+        url += `&category=${category}`;
+      }
+    } else if (category) {
+      url += `?category=${category}`;
     }
     try {
       const response = await axios({
@@ -215,7 +221,7 @@ const Settings: React.FC<ISettingsPagePros> = () => {
         url: url,
         headers: { 'Content-Type': 'multipart/form-data' },
       });
-      setTotalMonthlyAll(Number(Number(response.data.average).toFixed(2)));
+      setTotalMonthlyAll(Number(Number(response.data.total).toFixed(2)));
     } catch (error) {
       console.log(error);
     }
@@ -241,6 +247,162 @@ const Settings: React.FC<ISettingsPagePros> = () => {
     }
   };
 
+  const getTotalDemo = async (month?: number, category?: string) => {
+    let url: string = 'http://localhost:3000/api/recommender/weeklyTotal?userId=15';
+    let u15: number = 0;
+    let u16: number = 0;
+    if (month) {
+      url += `&month=${month}`;
+    }
+    if (category) {
+      url += `&category=${category}`;
+    }
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: url,
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+      u15 = Number(Number(response.data.total).toFixed(2));
+    } catch (error) {
+      console.log(error);
+    }
+    url = 'http://localhost:3000/api/recommender/weeklyTotal?userId=16';
+    if (month) {
+      url += `&month=${month}`;
+    }
+    if (category) {
+      url += `&category=${category}`;
+    }
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: url,
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+      u16 = Number(Number(response.data.total).toFixed(2));
+    } catch (error) {
+      console.log(error);
+    }
+    let total = Number(Number(u16 + u15)/2);
+    setDemoTotal(Number(total));
+
+    url = 'http://localhost:3000/api/recommender/monthlyTotal?userId=16';
+    if (month) {
+      url += `&month=${month}`;
+    }
+    if (category) {
+      url += `&category=${category}`;
+    }
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: url,
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+      u16 = Number(Number(response.data.total).toFixed(2));
+    } catch (error) {
+      console.log(error);
+    }
+    url = 'http://localhost:3000/api/recommender/monthlyTotal?userId=16';
+    if (month) {
+      url += `&month=${month}`;
+    }
+    if (category) {
+      url += `&category=${category}`;
+    }
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: url,
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+      u16 = Number(Number(response.data.total).toFixed(2));
+    } catch (error) {
+      console.log(error);
+    }
+    let total2 =Number(Number(u16 + u15)/2).toFixed(2);
+    setDemoTotalWeekly(Number(total2));
+  };
+
+  const getTotalDemoAvg = async (category?: string) => {
+    let url: string =
+      'http://localhost:3000/api/recommender/weeklyAverage?userId=15';
+    let u15: number = 0;
+    let u16: number = 0;
+    if (category) {
+      url += `&category=${category}`;
+    }
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: url,
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+      u15 = Number(Number(response.data.average).toFixed(2));
+    } catch (error) {
+      console.log(error);
+    }
+    url = 'http://localhost:3000/api/recommender/weeklyAverage?userId=16';
+    if (category) {
+      url += `&category=${category}`;
+    }
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: url,
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+      u16 = Number(Number(response.data.average).toFixed(2));
+    } catch (error) {
+      console.log(error);
+    }
+    let total = Number(u16 + u15) / 2;
+    setDemoAvgWeekly(Number(Number(total).toFixed(2)));
+  };
+
+  const getTotalDemoAvgMonth = async (month?: number, category?: string) => {
+    let url: string =
+      'http://localhost:3000/api/recommender/monthlyAverage?userId=15';
+    let u15: number = 0;
+    let u16: number = 0;
+    if (month) {
+      url += `&month=${month}`;
+    }
+    if (category) {
+      url += `&category=${category}`;
+    }
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: url,
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+      u15 = Number(Number(response.data.average).toFixed(2));
+    } catch (error) {
+      console.log(error);
+    }
+    url = 'http://localhost:3000/api/recommender/monthAverage?userId=16';
+    if (month) {
+      url += `&month=${month}`;
+    }
+    if (category) {
+      url += `&category=${category}`;
+    }
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: url,
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+      u16 = Number(Number(response.data.average).toFixed(2));
+    } catch (error) {
+      console.log(error);
+    }
+    let total = Number(u16 + u15) / 2;
+    setDemoAvg(Number(Number(total).toFixed(2)));
+  };
+
   useEffect(() => {
     getWeeklyAverage();
     getAverage();
@@ -252,6 +414,9 @@ const Settings: React.FC<ISettingsPagePros> = () => {
     getMonthlyTotal();
     getMonthlyTotalAll();
     getSpending();
+    getTotalDemo();
+    getTotalDemoAvg();
+    getTotalDemoAvgMonth();
   }, []);
 
   return (
@@ -327,7 +492,7 @@ const Settings: React.FC<ISettingsPagePros> = () => {
           style={{
             background: '#F1FCFF',
             display: 'grid',
-            gridTemplateColumns: '35% 65%',
+            gridTemplateColumns: '33% 33% 33%',
             overflow: 'hidden',
             margin: '1%',
           }}
@@ -344,7 +509,7 @@ const Settings: React.FC<ISettingsPagePros> = () => {
                 <BarChart
                   data={[
                     { x: 'This week', spending: totalWeekly },
-                    { x: 'All time', spending: average },
+                    { x: 'All time', spending: total },
                   ]}
                   margin={{
                     top: graph_height * 0.1,
@@ -362,21 +527,72 @@ const Settings: React.FC<ISettingsPagePros> = () => {
               </ResponsiveContainer>
             </CardContent>
           </Card>
-          <Card sx={{ ...long_style }}>
+          <Card sx={{ ...short_style }}>
             <CardHeader
-              title="Weekly Spending"
-              subheader={`Spending in the week of ${today.getFullYear()}-${
+              title="Weekly Spending Comparison"
+              subheader={`Spending comparison in the week of ${today.getFullYear()}-${
                 today.getMonth() + 1
               }-${today.getDate()}`}
             />
-            <CardContent></CardContent>
+            <CardContent>
+              <ResponsiveContainer width={graph_width} height={graph_height}>
+                <BarChart
+                  data={[
+                    { x: 'Me', spending: totalWeekly },
+                    { x: 'All Users', spending: totalDemoWeekly },
+                  ]}
+                  margin={{
+                    top: graph_height * 0.1,
+                    right: graph_width * 0.1,
+                    left: graph_width * 0.05,
+                    bottom: graph_height * 0.01,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="x" />
+                  <YAxis />
+                  <Tooltip />
+                  <Bar dataKey="spending" fill="#82ca9d" legendType="circle" />
+                </BarChart>
+              </ResponsiveContainer>
+            </CardContent>
+          </Card>
+          <Card sx={{ ...short_style }}>
+            <CardHeader
+              title="Weekly Average Receipt"
+              subheader={`Spending average in the week of ${today.getFullYear()}-${
+                today.getMonth() + 1
+              }-${today.getDate()}`}
+            />
+            <CardContent>
+              <ResponsiveContainer width={graph_width} height={graph_height}>
+                <BarChart
+                  data={[
+                    { x: 'Me', spending: averageWeekly },
+                    { x: 'All Users', spending: avgDemoWeekly },
+                  ]}
+                  margin={{
+                    top: graph_height * 0.1,
+                    right: graph_width * 0.1,
+                    left: graph_width * 0.05,
+                    bottom: graph_height * 0.01,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="x" />
+                  <YAxis />
+                  <Tooltip />
+                  <Bar dataKey="spending" fill="#F7A9A8" legendType="circle" />
+                </BarChart>
+              </ResponsiveContainer>
+            </CardContent>
           </Card>
         </div>
         <div
           style={{
             background: '#F1FCFF',
             display: 'grid',
-            gridTemplateColumns: '35% 65%',
+            gridTemplateColumns: '33% 33% 33%',
             overflow: 'hidden',
             margin: '1%',
           }}
@@ -393,8 +609,8 @@ const Settings: React.FC<ISettingsPagePros> = () => {
               <ResponsiveContainer width={graph_width} height={graph_height}>
                 <BarChart
                   data={[
-                    { x: 'This Month', spending: averageMonthly },
-                    { x: 'All time', spending: average },
+                    { x: 'This Month', spending: totalMonthly },
+                    { x: 'All time', spending: total },
                   ]}
                   margin={{
                     top: graph_height * 0.1,
@@ -412,15 +628,67 @@ const Settings: React.FC<ISettingsPagePros> = () => {
               </ResponsiveContainer>
             </CardContent>
           </Card>
-          <Card sx={{ ...long_style }}>
+          <Card sx={{ ...short_style }}>
             <CardHeader
-              title="Monthly Spending"
-              subheader={`Spending in the month of ${today.toLocaleString(
+              title="Monthly Spending Comparison"
+              subheader={`Spending comparison in the month of ${today.toLocaleString(
                 'default',
                 { month: 'long' }
               )} ${today.getFullYear()}`}
             />
-            <CardContent></CardContent>
+            <CardContent>
+              <ResponsiveContainer width={graph_width} height={graph_height}>
+                <BarChart
+                  data={[
+                    { x: 'Me', spending: totalMonthly },
+                    { x: 'All Users', spending: totalDemo },
+                  ]}
+                  margin={{
+                    top: graph_height * 0.1,
+                    right: graph_width * 0.1,
+                    left: graph_width * 0.05,
+                    bottom: graph_height * 0.01,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="x" />
+                  <YAxis />
+                  <Tooltip />
+                  <Bar dataKey="spending" fill="#82ca9d" legendType="circle" />
+                </BarChart>
+              </ResponsiveContainer>
+            </CardContent>
+          </Card>
+          <Card sx={{ ...short_style }}>
+            <CardHeader
+              title="Monthly Average Receipt"
+              subheader={`Spending average in the month of ${today.toLocaleString(
+                'default',
+                { month: 'long' }
+              )} ${today.getFullYear()}`}
+            />
+            <CardContent>
+              <ResponsiveContainer width={graph_width} height={graph_height}>
+                <BarChart
+                  data={[
+                    { x: 'Me', spending: averageMonthly },
+                    { x: 'All Users', spending: avgDemo },
+                  ]}
+                  margin={{
+                    top: graph_height * 0.1,
+                    right: graph_width * 0.1,
+                    left: graph_width * 0.05,
+                    bottom: graph_height * 0.01,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="x" />
+                  <YAxis />
+                  <Tooltip />
+                  <Bar dataKey="spending" fill="#F7A9A8" legendType="circle" />
+                </BarChart>
+              </ResponsiveContainer>
+            </CardContent>
           </Card>
         </div>
       </div>
